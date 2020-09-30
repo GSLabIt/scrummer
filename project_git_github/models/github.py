2fe77d36ff7cd1595ac462449ff1b251843151c4
Giovanni <giovanni@gslab.it>
2020-09-30 13:45:26 +0200

    -fix getattr

--- project_git_github/models/github.py
+++ project_git_github/models/github.py
@@ -77,8 +77,8 @@ class GitPayloadParser(models.AbstractModel):
     # -------------------------------------------
     def parse_github_payload(self, context):
         _logger.info(context)
-        method_name = self, "parse_github_%s" % context.action_type
-        parse_event_method = getattr('GitPayloadParser', method_name)
+        method_name = "parse_github_%s" % context.action_type
+        parse_event_method = getattr(self, method_name)
         return parse_event_method(context)
 
     # -------------------------------------------
